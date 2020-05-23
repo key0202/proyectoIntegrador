c2771e82f9d56052c21457fdb4af80c1c01630b5
david <contact@faztweb.com>
2020-05-23 12:42:35 -0500

    agregado login de acceso y Main

--- src/vistas/Login.java
+++ src/vistas/Login.java
@@ -1,6 +1,5 @@
 package vistas;
 
-import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.Icon;
@@ -9,14 +8,14 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Mikeyla
+ * @author DAVID
  */
-public class login extends javax.swing.JFrame {
+public class Login extends javax.swing.JFrame {
     
-    /**
-     * Creates new form login
-     */
-    public login() {
+    //enviar datos entre interfaces
+    public static String username = "";
+
+    public Login() {
         initComponents();
 
         //fijar tamanio fijo
@@ -24,17 +23,17 @@ public class login extends javax.swing.JFrame {
         setResizable(false);
 
         //titulo
-        setTitle("Registro Profesor");
+        setTitle("TeachTools");
         setLocationRelativeTo(null);
 
         //fondo imagen
-        ImageIcon fondo = new ImageIcon("src/imagenes/fondo_naranja.jpg"); // src/paquete/nombreImagen
+        ImageIcon fondo = new ImageIcon("src/imagenes/wallpaperPrincipal.jpg"); // src/paquete/nombreImagen
         Icon icono = new ImageIcon(fondo.getImage().getScaledInstance(jLabel_bgfondo.getWidth(),
                 jLabel_bgfondo.getHeight(), Image.SCALE_DEFAULT));
         jLabel_bgfondo.setIcon(icono);
         this.repaint();
     }
-    
+
     //metodo override llamado para cambiar icono de interfaz
     @Override
     public Image getIconImage() {
@@ -42,7 +41,6 @@ public class login extends javax.swing.JFrame {
         return retValue;
     }
     
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -55,13 +53,11 @@ public class login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        txtUsername = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        txtNombre = new javax.swing.JTextField();
-        txtDNI = new javax.swing.JTextField();
-        btnRegistrarse = new javax.swing.JButton();
-        btnVolver = new javax.swing.JButton();
         txtPassword = new javax.swing.JPasswordField();
+        btnAcceder = new javax.swing.JButton();
+        btnRegistro = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
         jLabel_bgfondo = new javax.swing.JLabel();
 
@@ -79,52 +75,63 @@ public class login extends javax.swing.JFrame {
         jLabel2.setText("Sistema de registro de asistencia y notas");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 400, -1));
 
-        jLabel3.setText("Nombre:");
+        jLabel3.setText("Ingrese su usuario:");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, -1, -1));
+        getContentPane().add(txtUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, 320, -1));
 
         jLabel4.setText("Contraseña:");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 210, -1, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 230, -1, -1));
+        getContentPane().add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 320, -1));
 
-        jLabel5.setText("DNI:");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, -1, -1));
-        getContentPane().add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, 320, -1));
-        getContentPane().add(txtDNI, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 310, 320, -1));
-
-        btnRegistrarse.setBackground(new java.awt.Color(102, 204, 255));
-        btnRegistrarse.setFont(new java.awt.Font("Comic Sans MS", 1, 13)); // NOI18N
-        btnRegistrarse.setText("REGISTRARSE");
-        btnRegistrarse.addActionListener(new java.awt.event.ActionListener() {
+        btnAcceder.setBackground(new java.awt.Color(102, 204, 255));
+        btnAcceder.setFont(new java.awt.Font("Comic Sans MS", 1, 13)); // NOI18N
+        btnAcceder.setText("ACCEDER");
+        btnAcceder.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRegistrarseActionPerformed(evt);
+                btnAccederActionPerformed(evt);
             }
         });
-        getContentPane().add(btnRegistrarse, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 360, 140, 40));
+        getContentPane().add(btnAcceder, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 330, 140, 40));
 
-        btnVolver.setText("VOLVER");
-        getContentPane().add(btnVolver, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 430, 140, 40));
-        getContentPane().add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, 320, -1));
+        btnRegistro.setFont(new java.awt.Font("Comic Sans MS", 1, 13)); // NOI18N
+        btnRegistro.setText("REGISTRARSE");
+        btnRegistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistroActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 400, 140, 40));
 
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel6.setText("© 2020 TecnoFast Enterprise");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 500, 400, -1));
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 490, 400, -1));
         getContentPane().add(jLabel_bgfondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 550));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnRegistrarseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarseActionPerformed
-        String nombre = txtNombre.getText().trim();
+    private void btnAccederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAccederActionPerformed
+        username = txtUsername.getText().trim().toUpperCase();
         String password = txtPassword.getText().trim();
-        String dni = txtDNI.getText().trim();
-        
-        //comprobar si todos los campos estan vacios        
-        if(nombre.isEmpty()||password.isEmpty() || dni.isEmpty() || dni.matches("[a-zA-Z_]+")){
+
+        //comprobar si todos los campos estan vacios
+        if (username.isEmpty() || password.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Debes llenar todos los campos correctamente");
-        }else{
-            //registrar profesor en bd
-            JOptionPane.showMessageDialog(null, "Registrado exitosamente");
+        } else {
+            //comprobar si existe usuario
+
+            //acceder al sistema
+            dispose();
+            Main acceso = new Main();
+            acceso.setVisible(true);
         }
-    }//GEN-LAST:event_btnRegistrarseActionPerformed
+    }//GEN-LAST:event_btnAccederActionPerformed
+
+    private void btnRegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistroActionPerformed
+        dispose();
+        Registrarse registrar = new Registrarse();
+        registrar.setVisible(true);
+    }//GEN-LAST:event_btnRegistroActionPerformed
 
     /**
      * @param args the command line arguments
@@ -143,36 +150,34 @@ public class login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new login().setVisible(true);
+                new Login().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnRegistrarse;
-    private javax.swing.JButton btnVolver;
+    private javax.swing.JButton btnAcceder;
+    private javax.swing.JButton btnRegistro;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel_bgfondo;
-    private javax.swing.JTextField txtDNI;
-    private javax.swing.JTextField txtNombre;
     private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
 }
